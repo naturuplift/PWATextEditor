93e0c9cba78e465d114d4b3b6ca31b7f3f2b6336
Arnaldo <arnaldosepulveda@gmail.com>
2024-03-21 01:16:44 -0300

    update src-sw

--- client/src-sw.js
+++ client/src-sw.js
@@ -4,6 +4,7 @@ const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
 const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.2.4/workbox-sw.js');
 
 precacheAndRoute(self.__WB_MANIFEST);
 
@@ -29,34 +30,20 @@ registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // Cache CSS, JavaScript, and Web Worker requests with a Stale-While-Revalidate strategy
 registerRoute(
-  ({ request }) => request.destination === 'style' || request.destination === 'script' || request.destination === 'worker',
+  // define callback function that filter requests to cache - case, JS and CSS files
+  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
   new StaleWhileRevalidate({
+    // Name of the cache storage.
     cacheName: 'asset-cache',
     plugins: [
+      // plugin will cache responses with headers to a maximum-age of 30 days
       new CacheableResponsePlugin({
         statuses: [0, 200],
       }),
       new ExpirationPlugin({
-        maxEntries: 60, // Limit number of files
-        maxAgeSeconds: 30 * 24 * 60 * 60, // Cache for 30 Days
-      }),
-    ],
-  })
-);
-
-// Cache image files with a Cache First strategy for long term caching
-registerRoute(
-  ({ request }) => request.destination === 'image',
-  new CacheFirst({
-    cacheName: 'image-cache',
-    plugins: [
-      new CacheableResponsePlugin({
-        statuses: [0, 200],
-      }),
-      new ExpirationPlugin({
-        maxEntries: 30, // Limit number of images
-        maxAgeSeconds: 60 * 24 * 60 * 60, // Cache for 60 Days
-      }),
+        maxEntries: 60,
+        maxAgeSeconds: 30 * 24 * 60 * 60, //30 Days
+      })
     ],
   })
 );
